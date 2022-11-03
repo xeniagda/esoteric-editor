688941a1e6e3aa0429c0d3294e306ca5067f0543
loovjo <jonathan.loov@gmail.com>
2022-11-03 20:49:38 +0100

    Detect process errors

--- glue/glue.py
+++ glue/glue.py
@@ -10,34 +10,51 @@ component_channels = {}
 RESET = "\033[0m"
 LOG_COLOR = "\033[38;5;69m"
 MSG_COLOR = "\033[38;5;195m"
+ERROR_COLOR = "\033[38;5;1m"
 TITLE = "\033[1m"
 
 async def process_stdout(proc, name):
-    while True:
-        receiver = ""
-        while receiver == "":
-            receiver = await proc.stdout.readexactly(5)
-        receiver = receiver[:-1]
-        data = (await proc.stdout.readuntil(b"\n"))[:-1]
+    try:
+        while True:
+            receiver = ""
+            while receiver == "":
+                receiver = await proc.stdout.readexactly(5)
+            receiver = receiver[:-1]
+            data = (await proc.stdout.readuntil(b"\n"))[:-1]
 
-        print(f"{MSG_COLOR}{name.decode('utf-8')}->{receiver.decode('utf-8')}{RESET} {repr(data)}")
-        await component_channels[receiver].put((name, data))
+            print(f"{MSG_COLOR}{name.decode('utf-8')}->{receiver.decode('utf-8')}{RESET} {repr(data)}")
+            await component_channels[receiver].put((name, data))
+    except asyncio.CancelledError as e:
+        print(f"{ERROR_COLOR}CANCELLED STDOUT PROCESSOR FOR [{name}]{RESET}")
+        return
 
-async def send_stdin(proc, ch):
-    async for (sender, data) in ch:
-        proc.stdin.write(sender + b"\n" + data + b"\n")
-        await proc.stdin.drain()
+async def send_stdin(proc, ch, name):
+    try:
+        async for (sender, data) in ch:
+            proc.stdin.write(sender + b"\n" + data + b"\n")
+            await proc.stdin.drain()
+    except asyncio.CancelledError as e:
+        print(f"{ERROR_COLOR}CANCELLED STDIN PROCESSOR FOR [{name}]{RESET}")
+        return
 
 async def pretty_stderr(proc, name):
-    while True:
-        line = await proc.stderr.readline()
-        print(f"{LOG_COLOR}LOG [{name}]{RESET} {line.decode('utf-8').strip()}")
+    try:
+        while True:
+            line = await proc.stderr.readline()
+            if line == b'':
+                print(f"{LOG_COLOR}CLOSED [{name}]{RESET}")
+                break
+            print(f"{LOG_COLOR}LOG [{name}]{RESET} {line.decode('utf-8').strip()}")
+    except asyncio.CancelledError as e:
+        print(f"{ERROR_COLOR}CANCELLED LOG FOR [{name}]{RESET}")
+        return
+
 
 async def start_component(cmd, name):
     print(f"{TITLE}Starting {name}{RESET}")
     name_b = name.encode("utf-8")
 
-    msg_channel = ch.create_channel()
+    msg_channel = ch.create_channel(100)
     component_channels[name_b] = msg_channel
 
     proc = await asyncio.create_subprocess_shell(
@@ -48,11 +65,19 @@ async def start_component(cmd, name):
         stderr=asyncio.subprocess.PIPE,
     )
 
-    await asyncio.gather(
-        process_stdout(proc, name_b),
-        send_stdin(proc, msg_channel),
-        pretty_stderr(proc, name),
-    )
+    stdout_processor = asyncio.create_task(process_stdout(proc, name_b))
+    stdin_processor = asyncio.create_task(send_stdin(proc, msg_channel, name))
+    stderr_logger = asyncio.create_task(pretty_stderr(proc, name))
+
+    await proc.wait()
+    if proc.returncode == 0:
+        print(f"{TITLE}{name} exited{RESET}")
+    else:
+        print(f"{TITLE}{ERROR_COLOR}{name} exited with status code {proc.returncode}{RESET}")
+
+    stdout_processor.cancel()
+    stdin_processor.cancel()
+    stderr_logger.cancel()
 
 async def main():
     os.setpgrp() # create new process group, become its leader
